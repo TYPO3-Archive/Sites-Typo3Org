f530941962ef40542103675d5a580eda047fad63
Christian Zenker <christian.zenker@599media.de>
2013-08-31 21:20:14 +0200

    [TASK] give Tomas Norre TYPO3 backend admin access to non-productive systems

--- scripts/post-install.d/50_enable.sh
+++ scripts/post-install.d/50_enable.sh
@@ -31,8 +31,8 @@ else
 	SOURCE_DB_PASS=`getLocalConfValue "typo_db_password" "${SOURCE_DIR}"`
 	SOURCE_DB_HOST=`getLocalConfValue "typo_db_host" "${SOURCE_DIR}"`
 
-	echo -n "Fix  some user settings for non-production systems (Christian Z, Nikola S, Mario M, Daniel L., Kay Strobach)"
-	echo "UPDATE be_users SET disable=0 WHERE uid IN (9,34, 50, 58, 151);" | mysql -u${SOURCE_DB_USER} -h${SOURCE_DB_HOST} -p${SOURCE_DB_PASS} ${SOURCE_DB_NAME}
+	echo -n "Fix  some user settings for non-production systems (Christian Z, Nikola S, Mario M, Daniel L., Kay Strobach, Tomas Norre)"
+	echo "UPDATE be_users SET disable=0 WHERE uid IN (9,34, 50, 58, 151, 153);" | mysql -u${SOURCE_DB_USER} -h${SOURCE_DB_HOST} -p${SOURCE_DB_PASS} ${SOURCE_DB_NAME}
 	echo -n "Make editor an admin on non-production systems (Thomas L.)"
 	echo "UPDATE be_users SET admin=1 WHERE uid IN (132);" | mysql -u${SOURCE_DB_USER} -h${SOURCE_DB_HOST} -p${SOURCE_DB_PASS} ${SOURCE_DB_NAME}
 	echo "done"
