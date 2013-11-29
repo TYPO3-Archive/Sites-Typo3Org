af60b5b167c345e2fe77270d1bedd610bdbd6c73
Christian Zenker <christian.zenker@aoemedia.de>
2013-11-29 15:02:10 +0100

    [TASK] use hashed passwords for TER test user
    SOAP API generates an exception on unhashed passwords

--- scripts/post-install.d/assets/ter_testusers.sql
+++ scripts/post-install.d/assets/ter_testusers.sql
@@ -1,6 +1,10 @@
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-alice',     'alice-password', 0, 0);
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-bob',       'bob-password',   0, 0);
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-eve',       'eve-password',   0, 0);
+# alices password is "alice-password"
+INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-alice',     '$P$CzHsG8cRdmY0mytc.fRql8YS0ZaSm4.', 0, 0);
+# bobs password is "bob-password"
+INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-bob',       '$P$CIwnePvuHIlmP3gIr8Hxw6rvSkxtZo.',   0, 0);
+# eves password is "eve-password"
+INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-eve',       '$P$CcvkV4CcmzlFTbmn/BIJd2rmXQrExO.',   0, 0);
 
 # this user has some password with special chars
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-weiredo',   '¢rÜpt|c',   0, 0);
+# password "¢rÜpt|c"
+INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-weiredo',   '$P$CMaGbWQ4WI7ROftZ1nmYkiKFqDYyxe1',   0, 0);
