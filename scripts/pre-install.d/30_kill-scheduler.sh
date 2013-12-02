599161b0b87e9f7684ac38ed3df81e19921d065f
Christian Zenker <christian.zenker@aoemedia.de>
2013-12-02 11:39:57 +0100

    [TASK] kill scheduler in internal environments before new installation
    There are some long running scheduler tasks that can break the deployment
    if they write to the newly created database.
    
    ERROR 1062 (23000) at line 24: Duplicate entry 'tx_scheduler-lastRun' for
    key 'entry_identifier'
    
    Just killing those tasks does not hurt because we are installing the a
    system anyways.

---
+++ scripts/pre-install.d/30_kill-scheduler.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+echo "#######################################################################"
+echo " disabled script make backup from production environment"
+echo "#######################################################################"
+exit 0;
+
+#####################################
+# apply command line arguments to local variable
+
+while getopts 'c:e:t:' OPTION ; do
+    case "${OPTION}" in
+		c)
+            CONCRETE_ENVIRONMENT="${OPTARG}"
+            ;;
+        e)
+            ENVIRONMENT="${OPTARG}"
+            ;;
+        t)
+            TARGETPATH="${OPTARG}"
+            ;;
+    esac
+done
+
+
+# Get absolute path to main directory
+ABSPATH=$(cd "${0%/*}" 2>/dev/null; cd ../..; echo "${PWD}/${0##*/}")
+SOURCE_DIR=`dirname "${ABSPATH}"`
+
+#######################################################################
+permissionsFixed=0
+INTERNALENVIRONMENTS=( t3o-deploy t3o-latest t3o-testing2)
+for internalEnvironment in ${INTERNALENVIRONMENTS[@]}
+do
+	if [ "${ENVIRONMENT}" = "${internalEnvironment}" ] ; then
+		echo "Remove system_installed.txt to avoid race conditions"
+		rm ${TARGETPATH}/system_installed.txt
+
+		echo "Killing scheduler for ${CONCRETE_ENVIRONMENT}"
+		pkill -f "${TARGETPATH}/htdocs/typo3/cli_dispatch.phpsh"
+		echo "done"
+	fi
+done
