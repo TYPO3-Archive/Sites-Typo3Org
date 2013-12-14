893a6ac2db81690f7a551c421afc18e93661d1ab
Christian Zenker <christian.zenker@599media.de>
2013-12-14 17:18:29 +0100

    add group for all test users so they can be used for cucumber

--- scripts/post-install.d/assets/ter_testusers.sql
+++ scripts/post-install.d/assets/ter_testusers.sql
@@ -1,9 +1,9 @@
 # alices password is "alice-password"
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-alice',     '$P$CzHsG8cRdmY0mytc.fRql8YS0ZaSm4.', 0, 0);
+INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-alice',     '$P$CzHsG8cRdmY0mytc.fRql8YS0ZaSm4.', '1', 0, 0);
 # bobs password is "bob-password"
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-bob',       '$P$CIwnePvuHIlmP3gIr8Hxw6rvSkxtZo.',   0, 0);
+INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-bob',       '$P$CIwnePvuHIlmP3gIr8Hxw6rvSkxtZo.', '1', 0, 0);
 # eves password is "eve-password"
-INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'autotest-eve',       '$P$CcvkV4CcmzlFTbmn/BIJd2rmXQrExO.',   0, 0);
+INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-eve',       '$P$CcvkV4CcmzlFTbmn/BIJd2rmXQrExO.', '1', 0, 0);
 
 # this user has some password with special chars
 # password "¢rÜpt|c"
@@ -11,7 +11,7 @@ INSERT INTO fe_users(pid, username, password, deleted, disable) VALUES ('11', 'a
 
 # this user is in the "TER reviewer" group
 # password "reviewer-password"
-INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-reviewer',   '$P$C2TnUnDot0jCZumvCk9OokcxFT3Qhd.', 22, 0, 0);
+INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-reviewer',   '$P$C2TnUnDot0jCZumvCk9OokcxFT3Qhd.', '1,22', 0, 0);
 # this user is in the "TER Administrator" group
 # password "admin-password"
-INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-admin',   '$P$CGt3Pr.uPi928UTlIir3qdPqGb7zvk/', 26, 0, 0);
+INSERT INTO fe_users(pid, username, password, usergroup, deleted, disable) VALUES ('11', 'autotest-admin',   '$P$CGt3Pr.uPi928UTlIir3qdPqGb7zvk/', '1,26', 0, 0);
