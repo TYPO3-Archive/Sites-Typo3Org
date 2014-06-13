a196a9f57fc59d2c58ae1807d97005776cba1621
Christian Zenker <christian.zenker@aoe.com>
2014-06-13 09:27:09 +0200

    do not clear realurl encode cache anymore
    no longer needed as solr does not spam it anymore #54000

--- scripts/post-install.d/20_clear_cache.sh
+++ scripts/post-install.d/20_clear_cache.sh
@@ -25,19 +25,6 @@ SOURCE_DIR=`dirname "${ABSPATH}"`
 
 #######################################################################
 
-getLocalConfValue() {
-	FIRST=$1
-	SECOND=$2
-
-	if [ ! -f "${SECOND}/htdocs/typo3conf/localconf.php" ] ; then
-		echo "File \"${SECOND}/htdocs/typo3conf/localconf.php\" not found" >&2
-		exit 1
-	fi
-
-	echo `grep "^\\\$${FIRST} =" ${SECOND}/htdocs/typo3conf/localconf.php | tail -1 | sed -e 's/.*= .//g' | sed -e 's/.;.*//g'`
-}
-#######################################################################
-
 #find "${SOURCE_DIR}/htdocs/typo3temp" -type f -not -path "${SOURCE_DIR}/htdocs/typo3temp/ter_*"  -delete
 
 if [ -d "${SOURCE_DIR}/htdocs/typo3temp/js_css_optimizer" ]; then
@@ -46,15 +33,3 @@ fi
 
 /usr/bin/php5 -q "${SOURCE_DIR}/htdocs/typo3/cli_dispatch.phpsh" lowlevel_cleaner tx_cachecleaner_cache -r --AUTOFIX --YES -s --optimize --tables sys_log
 /usr/bin/php5 -q "${SOURCE_DIR}/htdocs/typo3/cli_dispatch.phpsh" cleartypo3cache all > /dev/null
-
-
-SOURCE_DB_NAME=`getLocalConfValue "typo_db" "${SOURCE_DIR}"`
-SOURCE_DB_USER=`getLocalConfValue "typo_db_username" "${SOURCE_DIR}"`
-SOURCE_DB_PASS=`getLocalConfValue "typo_db_password" "${SOURCE_DIR}"`
-SOURCE_DB_HOST=`getLocalConfValue "typo_db_host" "${SOURCE_DIR}"`
-
-# tx_realurl_urlencodecache is quite huge (due to SOLR) and the major reason of
-# performance issues after production deployment.
-# @see http://forge.typo3.org/issues/51639
-# Could be removed after http://forge.typo3.org/issues/54000 has been fixed
-mysql -u${SOURCE_DB_USER} -p${SOURCE_DB_PASS} -h${SOURCE_DB_HOST} -D${SOURCE_DB_NAME} -e "TRUNCATE tx_realurl_urlencodecache;"
